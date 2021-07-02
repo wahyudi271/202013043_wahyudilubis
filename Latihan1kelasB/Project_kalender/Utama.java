1a8186198709d9a5a59a43fe71adcfad04135265
wahyudi271 <80767550+wahyudi271@users.noreply.github.com>
2021-07-02 16:21:49 +0700

    Add files via upload

---
+++ Latihan1kelasB/Project_kalender/Utama.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1kelasb;
+
+/**
+ *
+ * @author oryza
+ */
+public class Utama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        System.out.println(2+1);
+       
+        
+    }
+    
+}
